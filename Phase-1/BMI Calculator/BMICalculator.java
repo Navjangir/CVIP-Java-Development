c5529e61f3077db12166bfe7300f8aac8726d407
Navartna Jangir <110181146+Navjangir@users.noreply.github.com>
2023-12-19 11:47:41 +0530

    Update and rename Phase-1/BMICalculator.java to Phase-1/BMI Calculator/BMICalculator.java

---
+++ Phase-1/BMI Calculator/BMICalculator.java
@@ -0,0 +1,50 @@
+package Phase1.BMI_Cal;
+
+import java.util.Scanner;
+
+public class BmiCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("BMI Calculator 🧮");
+        System.out.println("Let's check your BMI");
+        System.out.println();
+
+        System.out.print("Enter your weight(kg): ");
+        double weight = sc.nextDouble();
+
+        System.out.print("Now, Enter your height(cm):");
+        double height = sc.nextDouble()/100;
+
+        System.out.println();
+
+        System.out.println("Hmm, Wait I'm Checking your BMI");
+
+        double bmi = getYourBmi(weight,height);
+
+        String formattedBmi = String.format("%.2f",bmi);//just for formatting the result upto 2 decimal points.
+
+        String result = interpretation(bmi);
+
+        System.out.println("Your BMI is  : "+formattedBmi);
+        System.out.println("Interpretation : "+result);
+    }
+
+    public static double getYourBmi(double weight,double height){
+        return weight/(height*height);
+    }
+
+    public static String interpretation(double bmi){
+        if(bmi<18.5){
+            return "Oops You are Underweight";
+        } else if (bmi >= 18.5 && bmi < 25) {
+            return "Good, your BMI is normal, Excellent";
+        } else if (bmi >= 25 && bmi < 30) {
+            return "Hmm, need to go to GYM, you are over-weight 🤭";
+        }
+        else {
+            return "Obese 🏋️‍♂️";
+        }
+    }
+}
